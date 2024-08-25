52c5ebc9402cdb5e88fd39d970dad3cf7abbae7c
ignaciodz <ignaciodiazbr@gmail.com>
2024-08-25 13:27:19 -0500

    feat(utils/data-structures): Configurar estructura de tabla alumnos

---
+++ src/admin/utils/data-structures/alumno-table-columns.js
@@ -0,0 +1,23 @@
+export const columnsAlumno = [
+  // { header: "ID", accessor: "id" },
+  { header: "Código", accessor: "codigo" },
+  { header: "DNI", accessor: "dni" },
+  { header: "Nombre", accessor: "nombre", minWidth: "min-w-[200px]" },
+  {
+    header: "Apellido Paterno",
+    accessor: "apellido_paterno",
+    minWidth: "min-w-[200px]",
+  },
+  {
+    header: "Apellido Materno",
+    accessor: "apellido_materno",
+    minWidth: "min-w-[200px]",
+  },
+  // { header: "Dirección", accessor: "direccion" },
+  // { header: "Telefono", accessor: "telefono" },
+  // { header: "Linkedin", accessor: "linkedin" },
+  // { header: "Correo", accessor: "correo" },
+  // { header: "Periodo", accessor: "periodo" },
+  // { header: "Carrera", accessor: "carrera" },
+  // { header: "Estado", accessor: "estado" },
+];
--- src/admin/utils/data-structures/index.js
+++ src/admin/utils/data-structures/index.js
@@ -2,3 +2,4 @@ export * from "./estado-table-columns";
 export * from "./carrera-table-columns";
 export * from "./periodo-table-columns";
 export * from "./grado-academico-table-columns";
+export * from "./alumno-table-columns";
