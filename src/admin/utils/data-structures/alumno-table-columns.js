3e4dbcae5bb5f134358edd2d5d427bff334f3392
ignaciodz <ignaciodiazbr@gmail.com>
2024-08-27 01:03:03 -0500

    feat(utils/data-structures): Modificar estructura de tabla alumnos

--- src/admin/utils/data-structures/alumno-table-columns.js
+++ src/admin/utils/data-structures/alumno-table-columns.js
@@ -1,23 +1,15 @@
 export const columnsAlumno = [
-  // { header: "ID", accessor: "id" },
   { header: "Código", accessor: "codigo" },
   { header: "DNI", accessor: "dni" },
-  { header: "Nombre", accessor: "nombre", minWidth: "min-w-[200px]" },
+  { header: "Nombre", accessor: "nombre", minWidth: "min-w-[160px]" },
   {
-    header: "Apellido Paterno",
+    header: "Ap. Paterno",
     accessor: "apellido_paterno",
-    minWidth: "min-w-[200px]",
+    minWidth: "min-w-[145px]",
   },
   {
-    header: "Apellido Materno",
+    header: "Ap. Materno",
     accessor: "apellido_materno",
-    minWidth: "min-w-[200px]",
+    minWidth: "min-w-[145px]",
   },
-  // { header: "Dirección", accessor: "direccion" },
-  // { header: "Telefono", accessor: "telefono" },
-  // { header: "Linkedin", accessor: "linkedin" },
-  // { header: "Correo", accessor: "correo" },
-  // { header: "Periodo", accessor: "periodo" },
-  // { header: "Carrera", accessor: "carrera" },
-  // { header: "Estado", accessor: "estado" },
 ];
