882727ab7958390d32fbc9f1d82e13446e01e2ec
ignaciodz <ignaciodiazbr@gmail.com>
2024-08-27 18:59:13 -0500

    feat(routes): Agregar rutas de usuarios en el Navigation

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SignIn } from "../auth/pages/Signin.jsx";
+import { SignIn } from "../auth/pages/SignIn.jsx";
 import { useAuth } from "../auth/hooks/useAuth.js";
 import adminRoutes from "./adminRoutes.jsx";
 import ProtectedRouteAdmin from "./ProtectedRouteAdmin.jsx";
+import { userRoutes } from "./userRoutes.jsx";
+import ProtectedRouteUser from "./ProtectedRouteUser.jsx";
 
 export const Navigation = () => {
   const { auth } = useAuth();
@@ -12,6 +14,20 @@ export const Navigation = () => {
         <Route path="/" element={<SignIn />} />
         {adminRoutes.map((route, index) => (
           <Route key={index} element={<ProtectedRouteAdmin auth={auth} />}>
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <route.layout>
+                  <route.page />
+                </route.layout>
+              }
+            />
+          </Route>
+        ))}
+
+        {userRoutes.map((route, index) => (
+          <Route key={index} element={<ProtectedRouteUser auth={auth} />}>
             <Route
               key={index}
               path={route.path}
