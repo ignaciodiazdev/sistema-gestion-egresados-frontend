b650eb1944cf4f0c4256a460a1e38ba0440fd7cd
ignaciodz <ignaciodiazbr@gmail.com>
2024-08-28 14:22:28 -0500

    feat(app): Envolver app con contexto de Modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,28 @@ import { AuthProvider } from "./auth/context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { ModalProvider } from "./shared/context/ModalContext";
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <Navigation />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={2700}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-          style={{ fontSize: "14px" }}
-        />
+        <ModalProvider>
+          <Navigation />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2700}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            style={{ fontSize: "14px" }}
+          />
+        </ModalProvider>
       </AuthProvider>
     </>
   );
