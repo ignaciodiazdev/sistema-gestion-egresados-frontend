6af0326e814033b9a5d397714c4a639c48f6bf9b
ignaciodz <ignaciodiazbr@gmail.com>
2024-08-29 17:30:03 -0500

    refactor(users/components): Modificar estructura para poner la nueva sección de empleos

--- src/users/components/inicio/CardAvisos.jsx
+++ src/users/components/inicio/CardAvisos.jsx
@@ -7,19 +7,22 @@ export const CardAvisos = () => {
     <CardUser>
       <div className="xl:min-h-[600px]">
         <div className="mb-6">
-          <h2 className="font-semibold mb-2">Aviso: Bolsa de Trabajo</h2>
+          <h2 className="font-semibold mb-2">Aviso: Ofertas Laborales</h2>
           <hr />
         </div>
         <div className="">
           <p className="text-sm p-2 bg-yellow-200 bg-opacity-10 rounded-md border-2 border-dashed border-yellow-400 mb-4 xl:mb-14">
-            Ahora puedes visitar la sección de Bolsa de Trabajo para encontrar
+            Ahora puedes visitar la sección de Ofertas Laborales para encontrar
             ofertas laborales!
           </p>
           <div className="hidden xl:block mb-14">
             <img src="/assets/svgs/job.svg" alt="" />
           </div>
           <div className="text-center w-full">
-            <Link className="block bg-[#e90066] text-white font-bold py-2 rounded-2xl hover:bg-[#e90069df] hover:shadow-sm hover:shadow-red-600">
+            <Link
+              to={"/empleos"}
+              className="block bg-[#e90066] text-white font-bold py-2 rounded-2xl hover:bg-[#e90069df] hover:shadow-sm hover:shadow-red-600"
+            >
               Ingresar
             </Link>
           </div>
